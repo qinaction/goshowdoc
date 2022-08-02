e2ebdc382eda970a36864c883970b1b5247fa182
qinaction <18437963602@163.com>
2022-08-02 18:49:09 +0800

    rt

---
+++ test/doc_test.go
@@ -0,0 +1,72 @@
+package test
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"goshowdoc"
+	"net/http"
+	"testing"
+)
+
+//请求绑定必须使用tool包中的BindJSON或者BindJSONNotWithValidate
+//若不想使用rsp.go 请使用ReplyJson或者ReplyCodeJson
+func TestGinResp(t *testing.T) {
+	r := gin.New()
+	UrlReq := "测试域名 ` http://xxx.cn`\n"
+	goshowdoc.EnableDoc(2, goshowdoc.Doc{
+		Url:      "https://XX=/api/item/updateByApi",
+		ApiKey:   "XX",
+		ApiToken: "XX",
+		UrlReq: UrlReq,
+	}, goshowdoc.Gin{})
+
+	r.POST("/user/list", userList)
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, "ping")
+	})
+	err := r.Run("127.0.0.1:8199")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+type docModel struct {
+	ID   uint   `gorm:"primary_key;comment:'数据id'" json:"id" req:"-"`
+	Name string `json:"name" gorm:"type:varchar(30);comment:姓名" req:"-"`
+	Sex  string `json:"sex" gorm:"type:varchar(30);comment:性别" req:"-"`
+	Age  int    `json:"age" gorm:"comment:年龄" req:"-"`
+}
+
+type u struct {
+	Name string `json:"name" gorm:"type:varchar(30);comment:姓名" req:"-"`
+	Sex  string `json:"sex" gorm:"type:varchar(30);comment:性别" req:"-"`
+}
+
+
+/*
+使用公共响应体
+*/
+func userList(c *gin.Context) {
+	var (
+		rsp Rsp
+		req u
+	)
+	err := goshowdoc.BindJSON(c, &req)
+	if err != nil {
+		rsp.ReplyFailOperation(c, err.Error())
+		return
+	}
+	//获取参数成功进行service、db操作
+	//操作完成返回
+	docModel := docModel{
+		ID: 1,
+		Name: "张三",
+		Sex: "男",
+		Age: 18,
+	}
+	goshowdoc.ReplyJson(c,docModel)
+	//输出json
+	c.JSON(http.StatusOK,docModel)
+}
+
+
---
+++ validator.go
@@ -0,0 +1,51 @@
+
+package goshowdoc
+
+import (
+	zhongwen "github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	"reflect"
+	"strings"
+)
+
+var Validate *validator.Validate
+var Trans ut.Translator
+
+func init() {
+	zh := zhongwen.New()
+	uni := ut.New(zh, zh)
+	Trans, _ = uni.GetTranslator("zh")
+
+	Validate = validator.New()
+	Validate.RegisterTagNameFunc(func(field reflect.StructField) string {
+		label := field.Tag.Get("comment")
+		if label == "" {
+			gorm := field.Tag.Get("gorm")
+			gormList := strings.Split(gorm, ";")
+			for _, v := range gormList {
+				if strings.Contains(v, "comment") {
+					comment := strings.Replace(strings.Split(v, ":")[1], "'", "", -1)
+					comments := strings.Split(comment, " ")
+					if len(comments) > 0 {
+						label = comments[0]
+					}
+				}
+			}
+			if label == "" {
+				return field.Name
+			}
+		}
+		return label
+	})
+	_=zh_translations.RegisterDefaultTranslations(Validate, Trans)
+}
+
+func TransError(err error) string {
+	s := err.(validator.ValidationErrors).Translate(Trans)
+	for _, value := range s {
+		return value
+	}
+	return "参数错误"
+}
